a523d6ce0b249d2c04fc5c687ef058d2fc8deb48
Mpekvoup <beknurtaiten@gmail.com>
2025-05-17 21:52:52 +0500

    Final fixes before deploy

--- main/views.py
+++ main/views.py
@@ -6,14 +6,12 @@ from django.views.decorators.http import require_GET
 
 
 def index(request):
-    # 1) Достаём код языка из сессии (его кладёт туда switch_language)
-    lang_code = request.session.get(translation.LANGUAGE_SESSION_KEY)
-    # 2) Если есть — активируем его непосредственно сейчас
+    # 1) Получаем код языка из сессии вручную
+    lang_code = request.session.get('django_language')
     if lang_code:
         translation.activate(lang_code)
         request.LANGUAGE_CODE = lang_code
 
-    # далее — ваша обычная логика
     if request.method == "POST":
         form = ConsultationRequestForm(request.POST)
         if form.is_valid():
@@ -28,11 +26,12 @@ def index(request):
 
     return render(request, "index.html", {"form": form})
 
+
 @require_GET
 def switch_language(request):
     lang = request.GET.get("lang")
-    nxt  = request.GET.get("next", "/")
+    nxt = request.GET.get("next", "/")
     if lang in dict(settings.LANGUAGES):
-        request.session[translation.LANGUAGE_SESSION_KEY] = lang
+        request.session['django_language'] = lang
         translation.activate(lang)
     return redirect(nxt)
